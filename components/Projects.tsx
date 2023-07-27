059fa89de6d912b7ec6bc29e3725976851e81e09
small-valley <kuzan1123@gmail.com>
2023-07-26 19:05:58 -0700

    delete project icon temporaly

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from "framer-motion";
+import Link from 'next/link';
 
 type Props = {
     projects: Project[],
@@ -18,7 +19,7 @@ export default function Projects({projects}: Props) {
         <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
             { projects?.map((project, index) => {
                 return <div key= {index} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
-                    <motion.img
+                    {/* <motion.img
                         initial={{
                             y: -300,
                             opacity: 0,
@@ -27,7 +28,7 @@ export default function Projects({projects}: Props) {
                         whileInView={{ opacity: 1, y: 0 }}
                         viewport={{ once: true }}
                         src={project.iconUrl}
-                        alt={project.alt} />
+                        alt={project.alt} /> */}
                     <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
                         <h4 className='text-4xl font-semibold text-center'>
                             <span className='underline decoration-[#F7AB0A]/50'>
@@ -49,7 +50,7 @@ export default function Projects({projects}: Props) {
                         <p className='text-lg text-center md:text-left'>
                             {project.summary}
                         </p>
-                        <a href={project.link}>Link to project source</a>
+                        <Link href={project.link}>Link to project source</Link>
                     </div>
                 </div>
             })}
